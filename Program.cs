f8eda3f4b1fd9b26cbc56a686033d8e76b158604
kirill <150036551+programmist-kirill@users.noreply.github.com>
2025-04-22 22:22:05 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+void calculating() {
+    string valut_user;
+    
+    Console.Write("какой валюты у вас деньги -");
+
+    valut_user = Console.ReadLine();
+    if (valut_user == "rus");
+    {
+        string count_cashe_user;
+        
+        Console.Write("Сколько у вас денег в этой валюте")
+    }
+}
+
+calculating();
---
+++ math.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10 
+c = 20
+d = 3
+
+result_chislitel = a + b + c
+result_znamenatel = result_chislitel / d
+
+result_chislitel = str(result_chislitel)
+result_znamenatel = str(result_znamenatel)
+
+print("result chislitel = " + result_chislitel + "\nresult znamenatel = " + result_znamenatel)
